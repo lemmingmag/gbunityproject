081a481ecb338f0d059a8ef458268d39b43fba78
lemmingmag <zhdamirov_in@outlook.com>
2018-05-28 20:22:48 +0500

    New dotnet core console proj created

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gbunityproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
